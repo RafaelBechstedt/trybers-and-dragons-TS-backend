2fa87e4800fbdffb6cec4841ba965389c59077b4
Rafael <bechstedt@hotmail.com>
2023-05-16 12:05:29 -0300

    Req 8

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,9 @@
+interface SimpleFighter {
+  lifePoints: number;
+  strength: number;
+
+  attack(enemy: SimpleFighter): void;
+  receiveDamage(attackPoints: number): number;  
+}
+
+export default SimpleFighter;
